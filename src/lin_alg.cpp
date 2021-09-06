ce21c5b89d97b12f8e293b9fc9538dbb1e58c4e1
jrexmo <30879519+jrexmo@users.noreply.github.com>
2021-09-06 13:54:05 -0700

    Update lin_alg.cpp

--- src/lin_alg.cpp
+++ src/lin_alg.cpp
@@ -1,28 +1,27 @@
 #include "lin_alg.h"
 
-std::vector<bool> operator* (const Sparse_Z2_Mat& M, const std::vector<bool>& v){
-	std::assert ( v.size() == M.cols);
-	std::vector<bool> out(M.rows, 0);
-	std::set<uint64_t>::iterator it;
-	bool b = 0;
-	uint64_t r = 0;
-	for (it = M.nzero.begin(); it != M.nzero.end(); ++it){
-		while( *it >= r * M.rows){
-			out[r++] = b; 
-			b = 0;
-		}
-		if ( v[ (*it)% M.rows] ){
-			b = !b;
-		}
+bool dot(const F2Vector v1, const F2Vector v2){
+	std::assert ( v1.size() == v2.size());
+	std::set<uint64_t> vec1 = v1.entries;
+	std::set<uint64_t> vec2 = v2.entries;
+
+	bool out = 0;
+	for (uint64_t i: vec1){
+		if (vec2.find(i) != vec2.end()) out = 1 - out;
 	}
 	return out;
 }
 
-bool dot(const std::vector<bool>& v1, const std::vector<bool>& v2){
-	std::assert ( v1.size() == v2.size());
-	bool out = 0;
-	for (int i = 0; i< v1.size(); ++i){
-		out ^= v1[i] & v2[i];
+F2Vector operator*(const SparseMat& M, const F2Vector& v){
+	F2Vector w;
+	w.size() = M.rows();
+	w.entries = {};
+	for (int i = 0; i < w.size(); i++) {
+		F2Vector row;
+		row.size() = M.cols();
+		row.entries = M.entry_map()[i];
+		if (dot(row, v)) w.insert(i);
 	}
-	return out;
+	return v;
 }
+
