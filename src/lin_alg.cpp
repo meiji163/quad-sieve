c0d8cbe2336c008d198b3625a5679f24778e59b1
jrexmo <30879519+jrexmo@users.noreply.github.com>
2021-09-06 14:16:52 -0700

    Update lin_alg.cpp

--- src/lin_alg.cpp
+++ src/lin_alg.cpp
@@ -1,7 +1,7 @@
 #include "lin_alg.h"
 
 bool dot(const F2Vector v1, const F2Vector v2){
-	std::assert ( v1.size() == v2.size());
+	std::assert (v1.size == v2.size);
 	std::set<uint64_t> vec1 = v1.entries;
 	std::set<uint64_t> vec2 = v2.entries;
 
@@ -14,14 +14,29 @@ bool dot(const F2Vector v1, const F2Vector v2){
 
 F2Vector operator*(const SparseMat& M, const F2Vector& v){
 	F2Vector w;
-	w.size() = M.rows();
+	w.size = M.rows;
 	w.entries = {};
 	for (int i = 0; i < w.size(); i++) {
 		F2Vector row;
-		row.size() = M.cols();
+		row.size = M.cols;
 		row.entries = M.entry_map()[i];
 		if (dot(row, v)) w.insert(i);
 	}
 	return v;
 }
 
+SparseMat operator*(const SparseMat& M1, const SparseMat& M2){
+	SparseMat R;
+	//TODO: include a check for size compatability
+	uint64_t a, b, c, d;
+	a = M1.rows;
+	b = M1.cols;
+	c = M2.rows;
+	d = M2.cols;
+	R.rows = a;
+	R.cols = d;
+	for (auto {i, s} : entry_map) {
+	
+	}
+	return R;
+}
